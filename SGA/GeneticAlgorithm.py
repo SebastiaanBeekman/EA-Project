ab75cd98d7c8508d8c67d39afbd3a2aa590d349c
NotUnlikeTheWaves <hleistra@gmail.com>
2024-05-30 14:25:24 +0200

    Add example use case (to be integrated)

--- SGA/GeneticAlgorithm.py
+++ SGA/GeneticAlgorithm.py
@@ -38,8 +38,9 @@ class GeneticAlgorithm:
 
 	def initialize_population( self ):
 		self.population = [Individual.initialize_uniform_at_random(self.fitness.dimensionality) for i in range(self.population_size)]
-        # Note including the adjacency list in the init call
-        # self.population = [Individual.initialize_from_square_assumption(self.fitness.dimensionality, self.fitness.adjacency_list) for i in range(self.population_size)]
+		for i in range(10):
+			# Note including the adjacency list in the init call
+			self.population[i] = Individual.initialize_from_square_assumption(self.fitness.dimensionality, self.fitness.adjacency_list)
 		for individual in self.population:
 			self.fitness.evaluate(individual)
 
